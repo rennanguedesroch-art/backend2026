5277ffcf97e6b76afccba8c9528816db53ff49d9
rennanguedesroch-art <rennanguedesroch@gmail.com>
2026-03-19 00:29:17 +0000

    aula 18/3/26

---
+++ src/forn.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<html>
+    <body>
+        <h1>Adivinha o login - gg </h1>
+        <form method="post" action="recebe.php">
+            Login<input type="text" id="login" name="login"><br>
+            Senha<input type="password" id="senha" name="senha"><br>
+            <input type="submit" value="Validar">
+
+  
+  
+        </form>
+    </body>
+</html>
